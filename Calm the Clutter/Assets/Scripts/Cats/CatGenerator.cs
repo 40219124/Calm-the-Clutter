1d16e71957a7f8a2905943a39900023744432472
Emma Parsley <emmaparsley98@gmail.com>
2020-08-23 15:07:10 +0100

    cat generator

---
+++ Calm the Clutter/Assets/Scripts/Cats/CatGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CatGenerator : MonoBehaviour
+{
+    [SerializeField]
+    List<Color> CatColours;
+    [SerializeField]
+    List<Color> CatNoseColours;
+
+    [SerializeField]
+    SpriteRenderer Body;
+    [SerializeField]
+    SpriteRenderer Tail;
+    [SerializeField]
+    SpriteRenderer Face;
+
+    void Awake()
+    {
+        GenerateCat();
+    }
+
+    void GenerateCat()
+    {
+        int catBod = Random.Range(0, CatColours.Count);
+        int catNose = Random.Range(0, CatNoseColours.Count);
+        Debug.Log($"catBod: {catBod}, catNose: {catNose}");
+
+        Body.color = CatColours[catBod];
+        Tail.color = CatColours[catBod];
+        Face.color = CatNoseColours[catNose];
+    }
+
+}
